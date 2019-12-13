3290d5eb66301f881677f64fb1bd1e95a1c2f7f2
Alejandro Marañes <alejandromaranes@gmail.com>
2019-12-13 12:06:19 +0100

    Empezando proyecto del 2 de febrero

---
+++ Bienestar-Digital-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bienestar-Digital-iOS
+//
+//  Created by Alejandro Marañés on 13/12/2019.
+//  Copyright © 2019 Alejandro Marañés. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
