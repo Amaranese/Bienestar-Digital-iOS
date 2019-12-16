62f116261b80a1bdf5a24b632d22fb83252c63c5
Alejandro Marañes <alejandromaranes@gmail.com>
2019-12-16 09:58:17 +0100

    4

--- Bienestar-Digital-iOS/ViewController.swift
+++ Bienestar-Digital-iOS/ViewController.swift
@@ -30,11 +30,40 @@ class ViewController: UIViewController {
                 let statusCode = response.response!.statusCode
                 if statusCode != 200 {
                     let alert = UIAlertController(title: "Error", message: jsonData["MESSAGE"] as! String, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    self.present(alert, animated: true)
+                } else {
+                    let arrayToken = response.result.value as! [String:Any]
+                    let userToken = arrayToken["MESSAGE"]!
+                    UserDefaults.standard.set(userToken, forKey: "token")
+                    UserDefaults.standard.set(true, forKey: "userLogged")
+                    let goMain = self.storyboard?.instantiateViewController(withIdentifier: "tabbar")
+                    self.present(goMain!, animated: true)
+                    
                 }
             }
         }
     }
-
-
+    @IBAction func Login(_ sender: UIButton) {
+        if textFieldEmail.text! == "" || textFieldPassword.text! == ""{
+            let alert = UIAlertController(title:"Error", message: "Has de rellenar todos los campos" as! String, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }else if !validateEmail(enteredEmail: textFieldEmail.text!){
+            let alert = UIAlertController(title: "Error", message: "El email no es correcto" as! String, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }else if textFieldPassword.text!.count < 8{
+            let alert = UIAlertController(title: "Error", message: "La contraseña no es correcta" as! String, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title:"Ok", style: .default, handler: nil))
+        }else{
+            print("Llego")
+        }
+        checkLogin()
+    }
+    func validateEmail(enteredEmail:String)-> Bool{
+        let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPredicate = NSPredicate(format:"Self Matches %@", emailFormat)
+        return emailPredicate.evaluate(with: enteredEmail)
+    }
 }
-
