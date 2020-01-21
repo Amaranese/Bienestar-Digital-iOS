a22b626c30dc3a3a58d772f53b78d235f91165d5
Alejandro Marañes <alejandromaranes@gmail.com>
2020-01-21 19:29:02 +0100

    Añadido el csv para su uso de cara a tenerlo conectado con la base de datos

--- Bienestar-Digital-iOS/Home/SeleccionarAppsVC.swift
+++ Bienestar-Digital-iOS/Home/SeleccionarAppsVC.swift
@@ -11,17 +11,41 @@ import UIKit
 class SeleccionarAppsVC: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
-    let apps = [
-        App(nombre: "Whatsapp", image: UIImage(named: "whatsapp")!),
-        App(nombre: "Instagram", image: UIImage(named: "instagram")!),
-        App(nombre: "Clock", image: UIImage(named: "clock")!),
-        App(nombre: "Facebook", image: UIImage(named: "facebook")!),
-        App(nombre: "Gmail", image: UIImage(named: "gmail")!),
-        App(nombre: "Chrome", image: UIImage(named: "chrome")!)
-    ]
+    var apps: [Usage] = []
+//    var apps = [
+//        App(nombre: "Whatsapp", image: UIImage(named: "whatsapp")!),
+//        App(nombre: "Instagram", image: UIImage(named: "instagram")!),
+//        App(nombre: "Clock", image: UIImage(named: "clock")!),
+//        App(nombre: "Facebook", image: UIImage(named: "facebook")!),
+//        App(nombre: "Gmail", image: UIImage(named: "gmail")!),
+//        App(nombre: "Chrome", image: UIImage(named: "chrome")!)
+//    ]
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.allowsMultipleSelection = true
+        let lines = self.loadDataCSV()
+        apps = self.loadUsages(lines: lines)
+        
+        
+    }
+    func loadDataCSV() -> [String] {
+        var usages: [String] = []
+        if let path = Bundle.main.path(forResource: "usage", ofType: "csv") {
+            do {
+                let data = try String(contentsOfFile: path, encoding: .utf8)
+                usages = data.components(separatedBy: .newlines)
+            } catch {
+                print(error)
+            }
+        }
+        return usages
+    }
+    func loadUsages(lines: [String]) -> [Usage] {
+        var usages: [Usage] = []
+        for i in 1...lines.count-2 {
+            usages.append(Usage(line: lines[i]))
+        }
+        return usages
     }
     @IBAction func clickContinuar(_ sender: Any) {
     }
@@ -38,8 +62,16 @@ extension SeleccionarAppsVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let app = apps[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "AppCell", for: indexPath) as! AppCell
-        cell.ivLogo.image = app.image
-        cell.tvNombre.text = app.nombre
+        cell.tvNombre.text = app.app
+        switch app.app {
+        case "Reloj": cell.ivLogo.image = UIImage(named: "clock")!
+        case "Instagram": cell.ivLogo.image = UIImage(named: "instagram")!
+        case "Whatsapp": cell.ivLogo.image = UIImage(named: "whatsapp")!
+        case "Facebook": cell.ivLogo.image = UIImage(named: "facebook")!
+        case "Gmail": cell.ivLogo.image = UIImage(named: "gmail")!
+        case "Chrome": cell.ivLogo.image = UIImage(named: "chrome")!
+        default: cell.ivLogo.image = UIImage(named: "clock")!
+        }
         return cell
     }
 }
---
+++ Bienestar-Digital-iOS/Usage.swift
@@ -0,0 +1,27 @@
+//
+//  Usage.swift
+//  Bienestar-Digital-iOS
+//
+//  Created by Alejandro Marañés on 21/01/2020.
+//  Copyright © 2020 Alejandro Marañés. All rights reserved.
+//
+
+import UIKit
+
+//Date,App,Event,Latitude,Longitude
+class Usage {
+    let date: String
+    let app: String
+    let event: String
+    let latitude: Double
+    let longitude: Double
+    
+    init(line: String) {
+        let fields = line.split(separator: ",")
+        self.date = String(fields[0])
+        self.app = String(fields[1])
+        self.event = String(fields[2])
+        self.latitude = Double(fields[3])!
+        self.longitude = Double(fields[4])!
+    }
+}
