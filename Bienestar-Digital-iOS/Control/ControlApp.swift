9f55014dd82fff716b5cd5381e607e73cdc44459
Alejandro Marañes <alejandromaranes@gmail.com>
2020-02-11 21:25:07 +0100

    no message

---
+++ Bienestar-Digital-iOS/Control/ControlApp.swift
@@ -0,0 +1,23 @@
+//
+//  ControlApp.swift
+//  Bienestar-Digital-iOS
+//
+//  Created by Alejandro Marañés on 11/02/2020.
+//  Copyright © 2020 Alejandro Marañés. All rights reserved.
+//
+
+import UIKit
+
+struct ControlAppConsulta: Codable {
+    let user_id: Int
+    let app_name: String
+}
+struct ControlAppConsultaResponse: Codable {
+    let max_time: Int
+}
+
+struct ControlAppActualizacion: Codable {
+    let user_id: Int
+    let app_name: String
+    let max_time: Int
+}
--- Bienestar-Digital-iOS/Control/ControlViewController.swift
+++ Bienestar-Digital-iOS/Control/ControlViewController.swift
@@ -7,12 +7,11 @@
 //
 
 import UIKit
+import Alamofire
 
 class ControlViewController: UIViewController {
     @IBOutlet weak var picker: UIPickerView!
     @IBOutlet weak var maxtime: UITextField!
-    @IBAction func save(_ sender: UIButton) {
-    }
     
     var apps: [App]!
     
@@ -23,14 +22,40 @@ class ControlViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    @IBAction func save(_ sender: UIButton) {
+        
+    }
     
-
-    
+    private func loadMaxTime(appName: String) {
+        let token = UserDefaults.standard.string(forKey: "token")!
+        let userID = UserDefaults.standard.integer(forKey: "user_id")
+        let url = "http://localhost:8888/Bienestar/public/index.php/api/control/read";
+        let data = ControlAppConsulta(user_id: userID, app_name: appName)
+        if let parameters = try? data.asDictionary() {
+            Alamofire.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default,
+                              headers: ["Content-Type": "application/json", "Authorization": token]).responseString { [weak self] response in
+                                guard let self = self else { return }
+                                if response.response!.statusCode == 200 {
+                                    if let result = response.result.value, result != "{}" {
+                                        let jsonData = result.data(using: .utf8)!
+                                        let resp: ControlAppConsultaResponse = try! JSONDecoder().decode(ControlAppConsultaResponse.self, from: jsonData)
+                                        self.maxtime.text = "\(resp.max_time)"
+                                    } else {
+                                        self.maxtime.text = ""
+                                    }
+                                }
+            }
+        }
+        
+    }
 
 }
 extension ControlViewController: UIPickerViewDelegate {
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return apps[row].name
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        loadMaxTime(appName: apps[row].name)
     }
 }
 extension ControlViewController: UIPickerViewDataSource {
